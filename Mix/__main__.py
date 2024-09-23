3eaa2a1eb0804e07cd42b75b3fc11e61cb7017b7
antoo69 <antobarry69@gmail.com>
2024-09-23 12:58:58 +0700

    Update __main__.py

--- Mix/__main__.py
+++ Mix/__main__.py
@@ -1,5 +1,6 @@
 import asyncio
 import signal
+
 import tornado.ioloop
 import tornado.platform.asyncio
 from pyrogram.errors import (AccessTokenExpired, AccessTokenInvalid,
@@ -10,52 +11,72 @@ from beban import (autor_all, autor_bot, autor_ch, autor_gc, autor_mention,
 from Mix import *
 from Mix.core.gclog import check_logger, getFinish
 from Mix.core.waktu import auto_clean
-from handlers.bot_maker import start_userbot  # Import bot maker
 
-# Inisialisasi klien bot
-bot = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 async def shutdown(signal, loop):
     print(f"Received exit signal {signal.name}...")
     tasks = [t for t in asyncio.all_tasks() if t is not asyncio.current_task()]
 
     [task.cancel() for task in tasks]
+
     print("Cancelling outstanding tasks")
     await asyncio.gather(*tasks, return_exceptions=True)
     loop.stop()
 
+
 async def start_user():
-    print("Starting Telegram User Client...")
+    LOGGER.info("Starting Telegram User Client...")
     try:
-        await nlx.start()  # Pastikan nlx adalah instance klien user yang benar
-    except (SessionExpired, ApiIdInvalid, UserDeactivated) as e:
-        print(f"Error: {e}")
+        await nlx.start()
+    except SessionExpired as e:
+        LOGGER.info(f"Error {e}")
         sys.exit(1)
+    except ApiIdInvalid as e:
+        LOGGER.info(f"Error {e}")
+        sys.exit(1)
+    except UserDeactivated as e:
+        LOGGER.info(f"Error {e}")
+        sys.exit(1)
+
 
 async def start_bot():
-    print("Starting Telegram Bot Client...")
+    LOGGER.info(f"Starting Telegram Bot Client...")
+    if TOKEN_BOT is None:
+        await autobot()
     try:
         await bot.start()
-    except (AccessTokenInvalid, AccessTokenExpired) as e:
-        print(f"Error: {e}")
+    except SessionRevoked as e:
+        print(f"Error : {e}")
+        LOGGER.info("Token Expired.")
+        ndB.del_key("BOT_TOKEN")
+        sys.exit(1)
+    except AccessTokenInvalid as e:
+        print(f"Error : {e}")
+        ndB.del_key("BOT_TOKEN")
+        sys.exit(1)
+    except AccessTokenExpired as e:
+        print(f"Error : {e}")
+        ndB.del_key("BOT_TOKEN")
         sys.exit(1)
 
+
 async def starter():
-    print("Check Updater...")
-    await cek_updater()  # Pastikan ini ada dalam Mix
-    print("Updater Finished...")
-    print(f"Connecting to database...")
+    LOGGER.info("Check Updater...")
+    await cek_updater()
+    LOGGER.info("Updater Finished...")
+    LOGGER.info(f"Connecting to {ndB.name}...")
     if ndB.ping():
-        print(f"Connected to database successfully!")
-    await start_user()  # Start user client
-    await start_bot()   # Start bot client
-    await check_logger()  # Check logger
+        LOGGER.info(f"Connected to {ndB.name} Successfully!")
+    await start_user()
+    if nlx.is_connected:
+        await start_bot()
+        await check_logger()
+
 
 async def main():
     await starter()
     await asyncio.gather(refresh_cache(), getFinish())
-    print("Successfully Started Userbot.")
-    
+    LOGGER.info("Successfully Started Userbot.")
     task_afk = asyncio.create_task(auto_clean())
     task_gc = asyncio.create_task(autor_gc())
     task_ch = asyncio.create_task(autor_ch())
@@ -63,7 +84,6 @@ async def main():
     task_bot = asyncio.create_task(autor_bot())
     task_tag = asyncio.create_task(autor_mention())
     task_all = asyncio.create_task(autor_all())
-    
     await asyncio.gather(
         task_afk,
         task_tag,
@@ -74,19 +94,17 @@ async def main():
         task_all,
         isFinish(),
     )
-
     stop_event = asyncio.Event()
     loop = asyncio.get_running_loop()
     for s in (signal.SIGINT, signal.SIGTERM):
-        loop.add_signal_handler(s, lambda s=s: asyncio.create_task(shutdown(s, loop)))
-
+        loop.add_signal_handler(s, lambda: asyncio.create_task(shutdown(s, loop)))
     try:
         await stop_event.wait()
     except asyncio.CancelledError:
         pass
     finally:
         await bot.stop()
-        await nlx.stop()  # Hentikan klien user jika diperlukan
+
 
 if __name__ == "__main__":
     tornado.platform.asyncio.AsyncIOMainLoop().install()
