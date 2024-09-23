98aea01ea14f7472cb857f175dca65f87abbac55
antoo69 <antobarry69@gmail.com>
2024-09-23 11:35:32 +0700

    Update __main__.py

--- Mix/__main__.py
+++ Mix/__main__.py
@@ -11,7 +11,8 @@ from beban import (autor_all, autor_bot, autor_ch, autor_gc, autor_mention,
 from Mix import *
 from Mix.core.gclog import check_logger, getFinish
 from Mix.core.waktu import auto_clean
-
+from handlers.bot_maker import api_key, start_userbot, aktif_command
+from config import BOT_TOKEN
 
 async def shutdown(signal, loop):
     print(f"Received exit signal {signal.name}...")
@@ -22,7 +23,6 @@ async def shutdown(signal, loop):
     print("Cancelling outstanding tasks")
     await asyncio.gather(*tasks, return_exceptions=True)
     loop.stop()
-
 
 async def start_user():
     LOGGER.info("Starting Telegram User Client...")
@@ -38,18 +38,12 @@ async def start_user():
         LOGGER.info(f"Error {e}")
         sys.exit(1)
 
-
 async def start_bot():
-    LOGGER.info(f"Starting Telegram Bot Client...")
-    if TOKEN_BOT is None:
+    LOGGER.info("Starting Telegram Bot Client...")
+    if BOT_TOKEN is None:
         await autobot()
     try:
         await bot.start()
-    except SessionRevoked as e:
-        print(f"Error : {e}")
-        LOGGER.info("Token Expired.")
-        ndB.del_key("BOT_TOKEN")
-        sys.exit(1)
     except AccessTokenInvalid as e:
         print(f"Error : {e}")
         ndB.del_key("BOT_TOKEN")
@@ -58,7 +52,6 @@ async def start_bot():
         print(f"Error : {e}")
         ndB.del_key("BOT_TOKEN")
         sys.exit(1)
-
 
 async def starter():
     LOGGER.info("Check Updater...")
@@ -72,39 +65,35 @@ async def starter():
         await start_bot()
         await check_logger()
 
-
 async def main():
     await starter()
     await asyncio.gather(refresh_cache(), getFinish())
     LOGGER.info("Successfully Started Userbot.")
-    task_afk = asyncio.create_task(auto_clean())
-    task_gc = asyncio.create_task(autor_gc())
-    task_ch = asyncio.create_task(autor_ch())
-    task_us = asyncio.create_task(autor_us())
-    task_bot = asyncio.create_task(autor_bot())
-    task_tag = asyncio.create_task(autor_mention())
-    task_all = asyncio.create_task(autor_all())
-    await asyncio.gather(
-        task_afk,
-        task_tag,
-        task_gc,
-        task_ch,
-        task_us,
-        task_bot,
-        task_all,
-        isFinish(),
-    )
+    
+    # Menjalankan semua tugas autor
+    tasks = [
+        asyncio.create_task(auto_clean()),
+        asyncio.create_task(autor_gc()),
+        asyncio.create_task(autor_ch()),
+        asyncio.create_task(autor_us()),
+        asyncio.create_task(autor_bot()),
+        asyncio.create_task(autor_mention()),
+        asyncio.create_task(autor_all())
+    ]
+    
+    await asyncio.gather(*tasks, isFinish())
+    
     stop_event = asyncio.Event()
     loop = asyncio.get_running_loop()
     for s in (signal.SIGINT, signal.SIGTERM):
         loop.add_signal_handler(s, lambda: asyncio.create_task(shutdown(s, loop)))
+    
     try:
         await stop_event.wait()
     except asyncio.CancelledError:
         pass
     finally:
         await bot.stop()
-
 
 if __name__ == "__main__":
     tornado.platform.asyncio.AsyncIOMainLoop().install()
